7a5c402afb9b9add97905b8c7c9c815cdc8f4195
Sandra Sulzberger <ssulzberger@gmail.com>
2024-06-11 07:19:06 +0200

    Initial Commit

---
+++ iLifeDesign/iLifeDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLifeDesignApp.swift
+//  iLifeDesign
+//
+//  Created by Sandra Sulzberger on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct iLifeDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
